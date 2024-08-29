901e0287cf2f717fa988705d0a348b7c1cef2d02
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-29 10:48:13 -0300

    feat: created user router

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from "fastify";
+import { UserUseCase } from "../usercases/user.usercase";
+import { UserCreate } from "../interfaces/user.interface";
+
+export async function userRoutes(fastify: FastifyInstance) {
+  const userUseCase = new UserUseCase();
+  fastify.post<{ Body: UserCreate }>("/", async (req, reply) => {
+    const { name, email } = req.body;
+    try {
+      const data = await userUseCase.create({
+        name,
+        email,
+      });
+      return reply.send(data);
+    } catch (error) {
+      reply.send(error);
+    }
+  });
+
+  fastify.get("/", async (req, reply) => {
+    try {
+    } catch (error) {}
+  });
+}
